7949bec93bb354ea76d080539ca787b55e8f8184
Gaurav Tewari <gauravtewari111@gmail.com>
2020-08-14 14:57:00 +0530

    basic

--- App.js
+++ App.js
@@ -1,21 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  Image,
+  TouchableOpacity,
+  SafeAreaView,
+  TouchableOpacityBase,
+  Alert,
+  Button,
+  Platform,
+} from "react-native";
 
 export default function App() {
+  const handlePress = () => {
+    console.log("yo");
+  };
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Button
+        title="Click me!"
+        onPress={() =>
+          Alert.alert("My Name", "My Message", [
+            {
+              text: "Yes",
+              onPress: () => {
+                console.log("Hello");
+              },
+            },
+            { text: "No" },
+          ])
+        }
+      />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
   },
 });
