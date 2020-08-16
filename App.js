6719183326c2c8ce151b54413d259d3babce2418
Gaurav Tewari <gauravtewari111@gmail.com>
2020-08-16 19:41:20 +0530

    picker element added

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useState } from "react";
 import {
   StyleSheet,
   Text,
@@ -12,6 +12,7 @@ import {
   Platform,
   Dimensions,
   View,
+  TextInput,
 } from "react-native";
 import {
   useDimensions,
@@ -24,11 +25,35 @@ import ViewImageContainer from "./app/screens/ViewScreenImage";
 import MessageScreen from "./app/screens/MessagesScreen";
 import AccountScreen from "./app/screens/AccountScreen";
 import ListingScreen from "./app/screens/ListingScreen";
+import Screen from "./app/components/Screen";
+import AppTextInput from "./app/components/AppTextInput";
+import AppPicker from "./app/components/AppPicker";
+
+const categories = [
+  { label: "Furniture", value: 1 },
+  { label: "Clothing", value: 2 },
+  { label: "Cameras", value: 3 },
+];
 
 export default function App() {
-  console.log("yo", useDimensions(), useDeviceOrientation());
+  const [category, setcategory] = useState("");
 
-  return <MessageScreen />;
+  return (
+    <Screen>
+      <AppTextInput
+        item={categories}
+        placeholder="Email"
+        icon="email"
+      ></AppTextInput>
+      <AppPicker
+        selectedItem={category}
+        onSelectItem={(item) => setcategory(item)}
+        icon="apps"
+        placeholder="Category"
+        items={categories}
+      ></AppPicker>
+    </Screen>
+  );
 }
 
 const styles = StyleSheet.create({
---
+++ app/components/AppPicker.js
@@ -0,0 +1,90 @@
+import React, { useState } from "react";
+import {
+  View,
+  TextInput,
+  StyleSheet,
+  Platform,
+  Modal,
+  Button,
+  FlatList,
+} from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import colors from "../config/colors";
+import defaultStyles from "../config/styles";
+import AppText from "./AppText";
+import { TouchableWithoutFeedback } from "react-native-gesture-handler";
+import PickerItem from "./PickerItem";
+
+const AppPicker = ({
+  icon,
+  placeholder,
+  items,
+  selectedItem,
+  onSelectItem,
+  ...otherProps
+}) => {
+  const [modalVisible, setModalVisible] = useState(false);
+
+  return (
+    <React.Fragment>
+      <TouchableWithoutFeedback onPress={() => setModalVisible(true)}>
+        <View style={styles.container}>
+          {icon && (
+            <MaterialCommunityIcons
+              name={icon}
+              size={20}
+              color={colors.medium}
+              style={styles.icon}
+            />
+          )}
+          <AppText style={styles.text}>
+            {selectedItem ? selectedItem.label : placeholder}
+          </AppText>
+          <MaterialCommunityIcons
+            name="chevron-down"
+            size={20}
+            color={colors.medium}
+          />
+        </View>
+      </TouchableWithoutFeedback>
+      <Modal visible={modalVisible} animationType="slide">
+        <Button
+          title="Close Button"
+          onPress={() => setModalVisible(false)}
+        ></Button>
+        <FlatList
+          data={items}
+          keyExtractor={(item) => item.value.toString()}
+          renderItem={({ item }) => (
+            <PickerItem
+              label={item.label}
+              onPress={() => {
+                setModalVisible(false);
+                onSelectItem(item);
+              }}
+            />
+          )}
+        />
+      </Modal>
+    </React.Fragment>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: colors.light,
+    borderRadius: 25,
+    flexDirection: "row",
+    width: "100%",
+    padding: 15,
+    marginVertical: 10,
+  },
+
+  icon: {
+    marginRight: 10,
+  },
+  text: {
+    flex: 1,
+  },
+});
+export default AppPicker;
